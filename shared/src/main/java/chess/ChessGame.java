70c403d6fd7df86a37ad85d5a2e64cd788ebb880
Lydia <let33@byu.edu>
2025-07-02 07:55:14 -0700

    Added hash codes

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -317,7 +318,17 @@ public class ChessGame {
 
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return turnNumber == chessGame.turnNumber && turn == chessGame.turn && Objects.equals(board, chessGame.board) && Objects.equals(validMoves, chessGame.validMoves);
+    }
 
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(turnNumber, turn, board, validMoves);
+    }
 }
